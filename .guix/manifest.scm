f13bb13a39d4757dc256ac06ceade665e5bcd5cc
Danny Milosavljevic <dannym@friendly-machines.com>
2026-08-29 03:34:14 +0200

    guix: Fix path

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,4 +1,4 @@
 (use-modules (guix packages))
 (use-modules (guix profiles))
 (packages->manifest (list (primitive-load (string-append (dirname (current-filename))
-                                                         "/guix.scm"))))
+                                                         "/../guix.scm"))))
